e6e8ecc0bcedd89582145f0098c10489dfb30b2f
MooManow <138873782+IT21Neo@users.noreply.github.com>
2023-09-27 22:26:43 +0700

    Edit the route way code

--- TrainToBangkok with no gui.py
+++ TrainToBangkok with no gui.py
@@ -1,37 +1,56 @@
-"""TrainToBangkok"""
 import pandas as pd
 
-#เก็บข้อมูล
-information = pd.read_csv(r"TrainToBangkok\TrainToBangkokDATA.csv", index_col="Station")
+# อ่านข้อมูลจากไฟล์ CSV
+information = pd.read_csv("TrainToBangkokDATA.csv", index_col="Station")
 
-#รวบรวมว่าสายใน dict ที่เป็น key ติดกับสายอะไรบ้าง โดยให้เป็น value
-line = {
-  "blueline" : {"redline", "lightblueline", "purpleline", "greenline"},
-  "lightblueline" : {"blueline", "greenline", "orangeline"},
-  "orangeline" : {"lightblueline"},
-  "littleyellowline" : {"lightblueline"},
-  "purpleline" : {"pinkline", "redline", "blueline"},
-  "redline" : {"purpleline", "blueline", "pinkline"},
-  "greenline" : {"pinkline", "blueline", "blackline", "lightblueline", "yellowline"},
-  "pinkline" : {"purpleline", "redline", "greenline"},
-  "yellowline" : {"blueline", "blackline", "greenline"},
-  "blackline" : {"blueline", "greenline", "yellowline"},
+# สร้างคำสั่งหาเส้นทางระหว่างสถานี
+def find_route(start_station, end_station, lines, line_connections):
+    start_line = information.loc[start_station]["Colorline"]
+    end_line = information.loc[end_station]["Colorline"]
+    
+    # ถ้าสายเดียวกัน
+    if start_line == end_line:
+        print(f"ใช้สาย {start_line}")
+    else:
+        # หาสายที่เชื่อมระหว่างสายเริ่มต้นและสายปลายทาง
+        common_lines = line_connections.get(start_line, set()) & line_connections.get(end_line, set())
+        
+        if common_lines:
+            # ถ้ามีสายรถไฟร่วมกัน
+            print(f"ใช้สายรถไฟ {', '.join(common_lines)} และเปลี่ยนที่สถานี {lines[start_line]}")
+        else:
+            print("ไม่มีเส้นทางรถไฟระหว่างสถานีนี้")
+
+# สร้างดิกชันนารีสำหรับการเชื่อมต่อระหว่างสายรถไฟ
+line_connections = {
+    "Blue": {"Lightblue", "Green", "Purple", "Red"},
+    "Lightblue": {"Blue", "Green", "Orange", "Littleyellow"},
+    "Orange": {"Lightblue"},
+    "Littleyellow": {"Lightblue"},
+    "Purple": {"Pink", "Red", "Blue"},
+    "Red": {"Purple", "Blue", "Pink"},
+    "Green": {"Pink", "Blue", "Black", "Lightblue", "Yellow"},
+    "Pink": {"Purple", "Red", "Green"},
+    "Yellow": {"Blue", "Black", "Green"},
+    "Black": {"Blue", "Green", "Yellow"},
 }
 
-def find_colorline(currentline, wantline, line):
-    #ใช้หาว่ารถไฟต้องผ่านสายไหนบ้าง
-    crossline = [((currentline).lower()+"line"), (wantline).lower()+"line"]
-    currentline = line[(currentline).lower()+"line"]
-    wantline = line[(wantline).lower()+"line"]
-    while len(currentline - wantline) == 0:
-        commute = []
-        for i in currentline:
-          if line[i] - 
+# สร้างดิกชันนารีสำหรับเก็บสถานีที่ต้องการเปลี่ยนสาย
+lines = {
+    "Blue": "B5",
+    "Lightblue": "LY1",
+    "Orange": "O3",
+    "Littleyellow": "LY3",
+    "Purple": "PU16",
+    "Red": "R6",
+    "Green": "G16",
+    "Pink": "PI1",
+    "Yellow": "Y1",
+    "Black": "BL23",
+}
 
-#รับ input มาทดลองโปรแกรม
-currentstation = input()
-wantstation = input()
-currentline = information.loc[currentstation]["Colorline"]
-wantline = information.loc[wantstation]["Colorline"]
+# รับค่าสถานีต้นทางและปลายทางจากผู้ใช้
+start_station = input("ป้อนสถานีต้นทาง: ")
+end_station = input("ป้อนสถานีปลายทาง: ")
 
-find_colorline(currentline, wantline, line)
+find_route(start_station, end_station, lines, line_connections)
