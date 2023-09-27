3f4b1e683b91408612d3335de159494240946305
Anawat <66070217@kmitl.ac.th>
2023-09-27 21:56:06 +0700

    Update TrainToBangkok with no gui.py

--- TrainToBangkok with no gui.py
+++ TrainToBangkok with no gui.py
@@ -6,22 +6,27 @@ information = pd.read_csv(r"TrainToBangkok\TrainToBangkokDATA.csv", index_col="S
 
 #รวบรวมว่าสายใน dict ที่เป็น key ติดกับสายอะไรบ้าง โดยให้เป็น value
 line = {
-  "blueline" : {"rightredline", "leftredline", "lightblueline", "purpleline", "greenline"},
+  "blueline" : {"redline", "lightblueline", "purpleline", "greenline"},
   "lightblueline" : {"blueline", "greenline", "orangeline"},
   "orangeline" : {"lightblueline"},
   "littleyellowline" : {"lightblueline"},
-  "purpleblueline" : {"pinkline", "leftredline", "blueline"},
-  "leftredblueline" : {"purpleline", "blueline"},
-  "rightredblueline" : {"pinkline", "blueline"},
-  "greenblueline" : {"pinkline", "blueline", "blackline", "lightblueline", "yellowline"},
-  "pinkblueline" : {"purpleline", "redline", "greenline"},
-  "yellowblueline" : {"blueline", "blackline", "greenline"},
-  "blackblueline" : {"blueline", "greenline", "yellowline"},
+  "purpleline" : {"pinkline", "redline", "blueline"},
+  "redline" : {"purpleline", "blueline", "pinkline"},
+  "greenline" : {"pinkline", "blueline", "blackline", "lightblueline", "yellowline"},
+  "pinkline" : {"purpleline", "redline", "greenline"},
+  "yellowline" : {"blueline", "blackline", "greenline"},
+  "blackline" : {"blueline", "greenline", "yellowline"},
 }
 
 def find_colorline(currentline, wantline, line):
     #ใช้หาว่ารถไฟต้องผ่านสายไหนบ้าง
-    crossline = [currentline, wantline]
+    crossline = [((currentline).lower()+"line"), (wantline).lower()+"line"]
+    currentline = line[(currentline).lower()+"line"]
+    wantline = line[(wantline).lower()+"line"]
+    while len(currentline - wantline) == 0:
+        commute = []
+        for i in currentline:
+          if line[i] - 
 
 #รับ input มาทดลองโปรแกรม
 currentstation = input()
