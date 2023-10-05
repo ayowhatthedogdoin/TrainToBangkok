5d4d8183324c4a7b52398fb8e3b2a2679980b6aa
Anawat <66070217@kmitl.ac.th>
2023-10-05 17:16:38 +0700

    fix data and add function to startstop
    still didnt finish shortestpath

--- TrainToBangkok with no gui.py
+++ TrainToBangkok with no gui.py
@@ -1,10 +1,10 @@
 """TrainToBangkok"""
 import pandas as pd
 from Data import ConnectStationData, stationpathstorage, Connectline
+from math import inf
 
 #เก็บข้อมูล
 information = pd.read_csv("Data\TrainToBangkokDATA.csv", index_col="Station")
-connect = ConnectStationData.connect
 
 #หาสถานีเชื่อมที่ใกล้ที่สุด
 def startstop(currentstation, word):
@@ -14,7 +14,6 @@ def startstop(currentstation, word):
     checkline = (stationnow.split())[0] #ใช้สร้างตัวเชคสีสาย
     checknum = (stationnow.split())[1]
 
-    print(stationnow)
     start = {}
     tod = ""     
     for i in linecan:
@@ -26,29 +25,49 @@ def startstop(currentstation, word):
                 tod = ""
                 continue
 
-        ConnectStationData.connect.update({word:start})
+    ConnectStationData.connect.update({word:start})
+
+    for i in linecan:
+        ConnectStationData.connect[i].update({word:ConnectStationData.connect[word][i]})
 
 #ใช้หาสถานีตอนนี้
 def setupstation(currentstation, wantstation):
     if "," in currentstation:
         ConnectStationData.connect["start"] = ConnectStationData.connect.pop[information.loc[currentstation]["Station ID"]]
         ConnectStationData.connect["end"] = ConnectStationData.connect.pop[information.loc[wantstation]["Station ID"]]
+        stationpathstorage.shortest["start"] = stationpathstorage.shortest.pop[information.loc[currentstation]["Station ID"]]
+        stationpathstorage.shortest["end"] = stationpathstorage.shortest.pop[information.loc[wantstation]["Station ID"]]
     else:
         startstop(currentstation, "start")
+        stationpathstorage.shortest.update({"start":0})
         startstop(wantstation, "end")
+        stationpathstorage.shortest.update({"end":inf})
+
+def forsort(key):
+    if key == "start":
+        return -inf
+    elif key == "end":
+        return inf
+    else:
+        return (list(ConnectStationData.connect.keys())).index(key)
 
 #หาเส้นทางที่สั้นที่สุดจากสถานีที่เป็นจุดเชื่อมต่างๆ
 def shortestpath(currentstation, wantstation):
-    allconnect = list(connect.keys())
-    """while len(allconnect) != 0:
-        print(allconnect)"""
+    allconnect = list(ConnectStationData.connect.keys())
+    allconnect.sort(key=forsort)
+    check = "start"
+    #while len(allconnect) != 0:
+    for i in ConnectStationData.connect[check]:
+        if ConnectStationData.connect[check][i] < stationpathstorage.shortest[i][0]:
+            stationpathstorage.shortest[i][0] = ConnectStationData.connect[check][i]
+            stationpathstorage.shortest[i][1] = check
+    
 
 #เชื่อมต่อ function ต่างๆเข้าด้วยกัน
 def main():
     currentstation = input("ตอนนี้คุณอยู่ที่สถานี : ")
     wantstation = input("คุณอยากไปที่สถานี : ")
     setupstation(currentstation, wantstation)
-    for i in range(len(ConnectStationData)):
-        numberstation = 0
-    print(ConnectStationData.connect.keys())
+    """shortestpath(currentstation, wantstation)"""
+    print(ConnectStationData.connect)
 main()
