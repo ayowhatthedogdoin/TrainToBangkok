bebffb0817f3ebd6658877058dff8168ee59e07c
ayowhatthedogdoin <66070219@kmitl.ac.th>
2023-10-13 21:54:50 +0700

    link page and change page calculator again...

--- templates/Calculator.js
+++ templates/Calculator.js
@@ -36,3 +36,47 @@ searchInp.addEventListener("keyup", () => {
 });
 
 selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
+
+document.getElementById('lines').addEventListener('change', function () {
+    var selectedLine = this.value;
+    var stationsSelect = document.getElementById('stations');
+    var stationsList = document.getElementById('stations-list');
+
+    // Clear existing stations
+    stationsList.innerHTML = '';
+
+    // Add stations dynamically based on the selected line
+    if (selectedLine === "Green Line (Sukhumvit Line)") {
+        populateStations(stationsSelect, ['Station A', 'Station B', 'Station C']);
+    } else if (selectedLine === 'sukhumvit') {
+        populateStations(stationsSelect, ['Station X', 'Station Y', 'Station Z']);
+    } else if (selectedLine === 'gold') {
+        populateStations(stationsSelect, ['Gold Station 1', 'Gold Station 2']);
+    } else {
+        hideStations();
+    }
+});
+
+document.getElementById('stations').addEventListener('change', function () {
+    var selectedStation = this.value;
+    alert('You selected: ' + selectedStation);
+});
+
+function populateStations(select, stationNames) {
+    // Clear existing options
+    select.innerHTML = '';
+    
+    stationNames.forEach(function (stationName) {
+        var option = document.createElement('option');
+        option.value = stationName.toLowerCase().replace(/\s/g, '_');
+        option.textContent = stationName;
+        select.appendChild(option);
+    });
+    
+    document.getElementById('stations-container').style.display = 'block';
+}
+
+// แก้ไขฟังก์ชัน hideStations เป็นแบบไม่ซ่อนองค์ประกอบแต่ละครั้ง
+function hideStations() {
+    document.getElementById('stations-container').style.display = 'none';
+}
