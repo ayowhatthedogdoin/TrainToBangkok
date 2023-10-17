205130db94a7191a8dc0ddc06f5ecfe0ddc76071
insisapim <66070322@kmitl.ac.th>
2023-10-17 19:56:14 +0700

    Update ConnectStationData.py

--- Data/ConnectStationData.py
+++ Data/ConnectStationData.py
@@ -1,116 +1,116 @@
 connect = {
     "PURPLE 11,PINK 1" : {
-        "PURPLE 15,RED 5" : 4,
-        "RED 11,PINK 13" : 13
+        "PURPLE 15,RED 5" : (4, 'purple'),
+        "RED 11,PINK 13" : (13, 'pink')
     },
 
     "RED 11,PINK 13" : {
-        "GREEN 8,PINK 15" : 2,
-        "PURPLE 11,PINK 1" : 13,
-        "BLUE 17,RED 6" : 5
+        "GREEN 8,PINK 15" : (2, 'pink'),
+        "PURPLE 11,PINK 1" : (13, 'pink'),
+        "BLUE 17,RED 6" : (5, 'red')
     },
 
     "GREEN 8,PINK 15" : {
-        "BLUE 20,GREEN 16" : 8,
-        "RED 11,PINK 13" : 2
+        "BLUE 20,GREEN 16" : (8, 'green'),
+        "RED 11,PINK 13" : (2, 'pink')
     },
 
     "BLUE 20,GREEN 16" : {
-        "BLUE 21,YELLOW 1" : 1,
-        "GREEN 8,PINK 15" : 8,
-        "BLUE 17,RED 6" : 3,
-        "GREEN 23,BLACK 1" : 7,
-        "BLUE 27,BLACK 3" : 7
+        "BLUE 21,YELLOW 1" : (1, 'blue'),
+        "GREEN 8,PINK 15" : (8, 'green'),
+        "BLUE 17,RED 6" : (3, 'blue'),
+        "GREEN 23,BLACK 1" : (7, 'green'),
+        "BLUE 27,BLACK 3" : (7, 'blue')
     },
 
     "GREEN 23,BLACK 1" : {
-        "BLUE 20,GREEN 16" : 7,
-        "BLUE 27,BLACK 3" : 2,
-        "LIGHTB 13,GREEN 25" : 2
+        "BLUE 20,GREEN 16" : (7, 'green'),
+        "BLUE 27,BLACK 3" : (2, 'black'),
+        "LIGHTB 13,GREEN 25" : (2, 'green')
     },
 
     "BLUE 28,GREEN 29" : {
-        "BLUE 27,BLACK 3" : 1,
-        "LIGHTB 13,GREEN 25" : 4,
-        "BLUE 32,LIGHTB 11" : 4,
-        "GREEN 40,YELLOW 23" : 11
+        "BLUE 27,BLACK 3" : (1, 'blue'),
+        "LIGHTB 13,GREEN 25" : (4, 'green'),
+        "BLUE 32,LIGHTB 11" : (4, 'blue'),
+        "GREEN 40,YELLOW 23" : (11, 'green')
     },
 
     "GREEN 40,YELLOW 23" : {
-        "BLUE 28,GREEN 29" : 11,
-        "YELLOW 11,BLACK 5" : 12
+        "BLUE 28,GREEN 29" : (11, 'green'),
+        "YELLOW 11,BLACK 5" : (12, ' yellow')
     },
     
     "BLUE 27,BLACK 3": {
-        "GREEN 23,BLACK 1" : 2,
-        "BLUE 20,GREEN 16" : 7,
-        "YELLOW 11,BLACK 5" : 2,
-        "BLUE 28,GREEN 29" : 1
+        "GREEN 23,BLACK 1" : (2, 'black'),
+        "BLUE 20,GREEN 16" : (7, 'blue'),
+        "YELLOW 11,BLACK 5" : (2, 'black'),
+        "BLUE 28,GREEN 29" : (1, 'blue')
     },
 
     "YELLOW 11,BLACK 5": {
-        "BLUE 27,BLACK 3" : 2,
-        "BLUE 21,YELLOW 1" : 10,
-        "GREEN 40,YELLOW 23" : 12
+        "BLUE 27,BLACK 3" : (2, 'black'),
+        "BLUE 21,YELLOW 1" : (10, 'yellow'),
+        "GREEN 40,YELLOW 23" : (12, 'yellow')
     },
 
     "BLUE 21,YELLOW 1": {
-        "BLUE 20,GREEN 16" : 1,
-        "YELLOW 11,BLACK 5" : 10
+        "BLUE 20,GREEN 16" : (1, 'blue'),
+        "YELLOW 11,BLACK 5" : (10, 'yellow')
     },
 
     "BLUE 5,LIGHTB 1": {
-        "BLUE 16,PURPLE 16" : 11,
-        "BLUE 32,LIGHTB 11" : 9,
-        "LIGHTB 3,LITTLEY 1" : 2
+        "BLUE 16,PURPLE 16" : (11, 'blue'),
+        "BLUE 32,LIGHTB 11" : (9, 'blue'),
+        "LIGHTB 3,LITTLEY 1" : (2, 'lightb')
     },
 
     "BLUE 16,PURPLE 16": {
-        "PURPLE 15,RED 5": 7,
-        "BLUE 17,RED 6": 1,
-        "BLUE 5,LIGHTB 1": 11
+        "PURPLE 15,RED 5": (7, 'purple'),
+        "BLUE 17,RED 6": (1, 'blue'),
+        "BLUE 5,LIGHTB 1": (11, 'blue')
     },
 
     "BLUE 17,RED 6": {
-        "BLUE 16,PURPLE 16": 1,
-        "BLUE 20,GREEN 16": 3,
-        "RED 11,PINK 13" : 5,
-        "PURPLE 15,RED 5" : 1
+        "BLUE 16,PURPLE 16": (1, 'blue'),
+        "BLUE 20,GREEN 16": (3, 'blue'),
+        "RED 11,PINK 13" : (5, 'red'),
+        "PURPLE 15,RED 5" : (1, 'red')
     },
 
     "BLUE 32,LIGHTB 11" : {
-        "BLUE 28,GREEN 29" : 4,
-        "LIGHTB 13,GREEN 25" : 2,
-        "LIGHTB 10,LITTLEY 12" : 1,
-        "BLUE 5,LIGHTB 1" : 9
+        "BLUE 28,GREEN 29" : (4, 'blue'),
+        "LIGHTB 13,GREEN 25" : (2, 'lightb'),
+        "LIGHTB 10,LITTLEY 12" : (1, 'lightb'),
+        "BLUE 5,LIGHTB 1" : (9, 'blue')
     },
 
     "LIGHTB 3,LITTLEY 1" : {
-        "BLUE 5,LIGHTB 1" : 2,
-        "LIGHTB 6,ORANGE 3" :3,
-        "LIGHTB 10,LITTLEY 12" : 11
+        "BLUE 5,LIGHTB 1" : (2, 'lightb'),
+        "LIGHTB 6,ORANGE 3" : (3, 'lightb'),
+        "LIGHTB 10,LITTLEY 12" : (11, 'littley')
     },
 
     "LIGHTB 6,ORANGE 3" : {
-        "LIGHTB 3,LITTLEY 1" : 3,
-        "LIGHTB 10,LITTLEY 12" : 4
+        "LIGHTB 3,LITTLEY 1" : (3, 'lightb'),
+        "LIGHTB 10,LITTLEY 12" : (4, 'lightb')
     },
 
     "LIGHTB 10,LITTLEY 12": {
-        "LIGHTB 6,ORANGE 3" : 4,
-        "LIGHTB 3,LITTLEY 1" : 11,
-        "BLUE 32,LIGHTB 11" : 1
+        "LIGHTB 6,ORANGE 3" : (4, 'lightb'),
+        "LIGHTB 3,LITTLEY 1" : (11, 'littley'),
+        "BLUE 32,LIGHTB 11" : (1, 'lightb')
     },
 
     "PURPLE 15,RED 5" : {
-        "PURPLE 11,PINK 1" : 4,
-        "BLUE 16,PURPLE 16" : 7,
-        "BLUE 17,RED 6" : 1
+        "PURPLE 11,PINK 1" : (4, 'purple'),
+        "BLUE 16,PURPLE 16" : (7, 'purple'),
+        "BLUE 17,RED 6" : (1, 'red')
     },
 
     "LIGHTB 13,GREEN 25" : {
-        "BLUE 28,GREEN 29" : 4,
-        "BLUE 32,LIGHTB 11" : 2,
-        "GREEN 23,BLACK 1" : 2
+        "BLUE 28,GREEN 29" : (4, 'green'),
+        "BLUE 32,LIGHTB 11" : (2, 'lightb'),
+        "GREEN 23,BLACK 1" : (2, 'green')
     }
 }
